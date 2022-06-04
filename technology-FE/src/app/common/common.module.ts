efde78369a912bc03b9938ccf356ff05e40a246d
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-05 01:04:14 +0700

    need fix header-navbar

--- technology-FE/src/app/common/common.module.ts
+++ technology-FE/src/app/common/common.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderBodyComponent } from './header-body/header-body.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent, HeaderBodyComponent],
-    exports: [
-        HeaderComponent,
-        FooterComponent,
-        HeaderBodyComponent
-    ],
+    declarations: [HeaderComponent, FooterComponent, HeaderBodyComponent, HeaderNavbarComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    HeaderBodyComponent,
+    HeaderNavbarComponent
+  ],
     imports: [
 
     ]
