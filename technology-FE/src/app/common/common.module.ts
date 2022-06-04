83e17e071c807b79f0de9c99616d241eab29f021
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-05 00:28:02 +0700

    done header-top and header-middle

--- technology-FE/src/app/common/common.module.ts
+++ technology-FE/src/app/common/common.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderBodyComponent } from './header-body/header-body.component';
 
 
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent],
-  exports: [
-    HeaderComponent,
-    FooterComponent
-  ],
+    declarations: [HeaderComponent, FooterComponent, HeaderBodyComponent],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        HeaderBodyComponent
+    ],
     imports: [
 
     ]
