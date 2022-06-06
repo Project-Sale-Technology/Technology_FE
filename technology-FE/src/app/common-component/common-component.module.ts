8c1128adaf2e111a11f3e2d66e5bacb46062098f
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-07 00:01:26 +0700

    pull module component

--- technology-FE/src/app/common-component/common-component.module.ts
+++ technology-FE/src/app/common-component/common-component.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {HeaderTopComponent} from "./header-top/header-top.component";
+import {HeaderNavbarComponent} from "./header-navbar/header-navbar.component";
+import {HeaderMiddleComponent} from "./header-middle/header-middle.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderTopComponent, HeaderNavbarComponent, HeaderMiddleComponent, FooterComponent],
+  exports: [
+    HeaderTopComponent,
+    HeaderNavbarComponent,
+    HeaderMiddleComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
