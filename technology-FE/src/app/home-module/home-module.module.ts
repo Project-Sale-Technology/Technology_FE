438b2ab1998a01ef343d98426601ce00ae2f99c4
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-06 16:20:16 +0700

    fix

--- technology-FE/src/app/home-module/home-module.module.ts
+++ technology-FE/src/app/home-module/home-module.module.ts
@@ -4,15 +4,13 @@ import { HomeModuleRoutingModule } from './home-module-routing.module';
 import { HomeContactComponent } from './home-contact/home-contact.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import {CommonModule} from "../common/common.module";
+import {FooterComponent} from "../common-component/footer/footer.component";
 
 
 @NgModule({
-  declarations: [HomeContactComponent, HomeComponent, AboutUsComponent],
+  declarations: [HomeContactComponent, HomeComponent, AboutUsComponent, FooterComponent],
   imports: [
-    CommonModule,
-    HomeModuleRoutingModule,
-    CommonModule,
+    HomeModuleRoutingModule
   ]
 })
 export class HomeModuleModule { }
