286d340e13250a637e2ce89d8a793769d9da6fae
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-07 16:27:52 +0700

    done login page

--- technology-FE/src/app/account-module/login/login.component.ts
+++ technology-FE/src/app/account-module/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+   /* set name for current page */
+  currentPage = 'Login';
+
   constructor() { }
 
   ngOnInit(): void {
