8d5ea2f67bddc9210c3162762eb2a2ec12d90158
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-07 13:38:29 +0700

    done header-footer

--- technology-FE/src/app/common-component/common-component.module.ts
+++ technology-FE/src/app/common-component/common-component.module.ts
@@ -4,15 +4,17 @@ import {HeaderTopComponent} from "./header-top/header-top.component";
 import {HeaderNavbarComponent} from "./header-navbar/header-navbar.component";
 import {HeaderMiddleComponent} from "./header-middle/header-middle.component";
 import {FooterComponent} from "./footer/footer.component";
+import { HeaderFooterComponent } from './header-footer/header-footer.component';
 
 @NgModule({
-  declarations: [HeaderTopComponent, HeaderNavbarComponent, HeaderMiddleComponent, FooterComponent],
-  exports: [
-    HeaderTopComponent,
-    HeaderNavbarComponent,
-    HeaderMiddleComponent,
-    FooterComponent
-  ],
+  declarations: [HeaderTopComponent, HeaderNavbarComponent, HeaderMiddleComponent, FooterComponent, HeaderFooterComponent],
+    exports: [
+        HeaderTopComponent,
+        HeaderNavbarComponent,
+        HeaderMiddleComponent,
+        FooterComponent,
+        HeaderFooterComponent
+    ],
   imports: [
     CommonModule
   ]
