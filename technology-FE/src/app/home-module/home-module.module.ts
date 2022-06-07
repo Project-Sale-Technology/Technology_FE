74d59175234da757824ccfd7cc1fd504d0f2f4a5
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-07 14:09:16 +0700

    fix bug component

--- technology-FE/src/app/home-module/home-module.module.ts
+++ technology-FE/src/app/home-module/home-module.module.ts
@@ -4,13 +4,12 @@ import { HomeModuleRoutingModule } from './home-module-routing.module';
 import { HomeContactComponent } from './home-contact/home-contact.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import {FooterComponent} from "../common-component/footer/footer.component";
 import {CommonComponentModule} from "../common-component/common-component.module";
 import {CommonModule} from "@angular/common";
 
 
 @NgModule({
-  declarations: [HomeContactComponent, HomeComponent, AboutUsComponent, FooterComponent],
+  declarations: [HomeContactComponent, HomeComponent, AboutUsComponent],
   imports: [
     HomeModuleRoutingModule,
     CommonComponentModule,
