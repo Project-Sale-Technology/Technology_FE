fb4354da058e568e7535d5eabe3af6f8d52cfbe0
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-08 09:57:59 +0700

    done register page

--- technology-FE/src/app/account-module/register/register.component.ts
+++ technology-FE/src/app/account-module/register/register.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  /* Set name for current page */
+  currentPage = 'Registration';
+
   constructor() { }
 
   ngOnInit(): void {
