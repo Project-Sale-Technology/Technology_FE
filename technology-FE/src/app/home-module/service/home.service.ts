591d5c1aa9932a3c6ba2d3020b3728e08354f074
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-11 11:24:48 +0700

    done show products and categories

--- technology-FE/src/app/home-module/service/home.service.ts
+++ technology-FE/src/app/home-module/service/home.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../../model/Product";
+import {Category} from "../../model/Category";
+
+const API_URL = "http://localhost:8080/home";
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +12,13 @@ import {HttpClient} from "@angular/common/http";
 export class HomeService {
 
   /* Get all products */
-  getAllProducts() {
+  getAllProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`${API_URL}/products`);
+  }
 
+  /* Get categories */
+  getAllCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(`${API_URL}/category`);
   }
 
   constructor(private httpClient: HttpClient) { }
