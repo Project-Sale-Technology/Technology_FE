d3a192580b2a5a6cdf9d939d1df41e92c8d3efbc
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-25 21:15:25 +0700

    done login and need add session when login successfully

--- technology-FE/src/app/account-module/login/login.component.ts
+++ technology-FE/src/app/account-module/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AccountService} from "../service/account.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-   /* set name for current page */
+  /* set name for current page */
   currentPage = 'Login';
+  alertMessageError = false;
 
-  constructor() { }
+  /* Initialize form login */
+  formLogin: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder , private accountService: AccountService , private router: Router) {
   }
 
+  ngOnInit(): void {
+    /* Set variable and validate for login form */
+    this.formLogin = this.fb.group({
+      email: ['', Validators.compose([Validators.required,
+        Validators.minLength(17), Validators.pattern("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")])],
+      password: ['' , Validators.compose([Validators.required , Validators.minLength(4)])]
+    })
+  }
+
+  handleLogin() {
+    const email = this.email.value;
+    const password = this.password.value;
+    this.accountService.handleLogin(email , password).subscribe(data=> {
+      if(data == null) {
+        this.formLogin.reset();
+        this.alertMessageError = true;
+      } else {
+        this.router.navigateByUrl("");
+      }
+    })
+  }
+
+  /* Getter  for form login */
+  get email() {
+    return this.formLogin.get('email');
+  }
+
+  get password() {
+    return this.formLogin.get('password');
+  }
 }
