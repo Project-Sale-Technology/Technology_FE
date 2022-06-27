71ba633d61388c50eaa7e64c4504b2d668393c0d
Nguyen Hoang Anh Tuan <nguyenhanhtuan1206@gmail.com>
2022-06-28 00:41:15 +0700

    need fix send to email

--- technology-FE/src/app/account-module/forgot-password/forgot-password.component.ts
+++ technology-FE/src/app/account-module/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ForgotPasswordService} from "./service/forgot-password.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {checkUserExisting} from "../validate/ValidateCheckUserExisting";
 
 @Component({
   selector: 'app-forgot-password',
@@ -10,9 +13,40 @@ export class ForgotPasswordComponent implements OnInit {
   /* Set name for current page*/
   currentPage = 'Forgot Password';
 
-  constructor() { }
+  /* Initialize form */
+  formSendEmail: FormGroup;
 
-  ngOnInit(): void {
+  /* Set data to send mail */
+  token: string;
+  checkSentToMail: boolean;
+
+  constructor(private forgotPasswordService: ForgotPasswordService
+    , private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    /* Set variables for form */
+    this.formSendEmail = this.fb.group({
+      email: ['',
+        Validators.compose([Validators.required, Validators.pattern("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"), Validators.minLength(17)])],
+    })
+  }
+
+  sendRequest() {
+    const email = this.email.value;
+    console.log(email);
+    this.forgotPasswordService.processPassword(email).subscribe(token => {
+      this.token = token;
+      /* Set token for reset password */
+      window.localStorage.setItem('reset_token' , token);
+      this.checkSentToMail = true;
+    } , error => {
+      this.checkSentToMail = false;
+    })
+  }
+
+  /* Getter variable for form */
+  get email() {
+    return this.formSendEmail.get('email');
+  }
 }
